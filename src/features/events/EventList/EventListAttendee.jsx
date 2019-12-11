523f8e53bc526ae623e188cb5a282818f3be26c0
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2019-12-11 10:10:13 +0100

    feature: Add the components with static data

--- src/features/events/EventList/EventListAttendee.jsx
+++ src/features/events/EventList/EventListAttendee.jsx
@@ -1,19 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { List, Image } from 'semantic-ui-react';
 
-class EventListAttendee extends Component {
-	render() {
-		return (
-			<List.Item>
-				<Image
-					as="a"
-					size="mini"
-					circular
-					src="https://randomuser.me/api/portraits/women/42.jpg"
-				/>
-			</List.Item>
-		);
-	}
-}
+const EventListAttendee = ({ attendee }) => {
+	return (
+		<List.Item>
+			<Image as="a" size="mini" circular src={attendee.photoURL} />
+		</List.Item>
+	);
+};
 
 export default EventListAttendee;
