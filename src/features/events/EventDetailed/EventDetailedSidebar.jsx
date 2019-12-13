e24346aecb16288572d058e769376a33d8f5832f
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2019-12-14 00:57:16 +0100

    chore: Add static date for event detailed page

--- src/features/events/EventDetailed/EventDetailedSidebar.jsx
+++ src/features/events/EventDetailed/EventDetailedSidebar.jsx
@@ -1,37 +1,44 @@
-import React, { Fragment } from 'react'
-import { Segment, Item, Label } from 'semantic-ui-react'
+import React, { Fragment } from 'react';
+import { Segment, Item, Label, List, Image } from 'semantic-ui-react';
 
-export const EventDetailedSidebar = () => {
-  return (
-    <Fragment>
-      <Segment
-        textAlign='center'
-        style={{ border: 'none' }}
-        attached='top'
-        secondary
-        inverted
-        color='teal'
-      >
-        2 People Going
-      </Segment>
-      <Segment attached>
-        <Item.Group divided>
-          <Item style={{ position: 'relative' }}>
-            <Label
-              style={{ position: 'absolute' }}
-              color='orange'
-              ribbon='right'
-            >
-              Host
-            </Label>
-            <Item.Image size='mini' src='/assets/user.png' />
-            <Item.Content verticalAlign='middle'>
-              <Item.Header as='h3'>Attendee Name</Item.Header>
-            </Item.Content>
-          </Item>
-        </Item.Group>
-      </Segment>
-    </Fragment>
-  )
-}
-export default EventDetailedSidebar
+export const EventDetailedSidebar = ({ attendees }) => {
+	const isHost = false;
+	return (
+		<Fragment>
+			<Segment
+				textAlign="center"
+				style={{ border: 'none' }}
+				attached="top"
+				secondary
+				inverted
+				color="teal"
+			>
+				{attendees && attendees.length}{' '}
+				{attendees && attendees.length === 1 ? 'Person' : 'People'} Going
+			</Segment>
+			<Segment attached>
+				<List relaxed divided>
+					{attendees &&
+						attendees.map(attendee => (
+							<Item key={attendee.id} style={{ position: 'relative' }}>
+								{isHost && (
+									<Label
+										style={{ position: 'absolute' }}
+										color="orange"
+										ribbon="right"
+									>
+										Host
+									</Label>
+								)}
+								<Image size="mini" circular src={attendee.photoURL} />
+								<Item.Content verticalAlign="middle">
+									<Item.Header as="h3">{attendee.name}</Item.Header>
+								</Item.Content>
+							</Item>
+						))}
+				</List>
+			</Segment>
+		</Fragment>
+	);
+};
+export default EventDetailedSidebar;
