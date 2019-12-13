2b59b0934ed02b8e38989af4b4384a373fdecb15
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2019-12-13 19:43:17 +0100

    feature: Connect event actions to event dashboard

--- src/features/events/EventDashboard/EventDashboard.jsx
+++ src/features/events/EventDashboard/EventDashboard.jsx
@@ -1,62 +1,11 @@
 import React, { Component } from 'react';
 import { Grid, Button } from 'semantic-ui-react';
+import { connect } from 'react-redux';
 import EventList from '../EventList/EventList';
 import EventForm from '../EventForm/EventForm';
 
-const events = [
-	{
-		id: '1',
-		title: 'Trip to Tower of London',
-		date: '2018-03-27T11:00:00+00:00',
-		category: 'culture',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
-		city: 'London, UK',
-		venue: "Tower of London, St Katharine's & Wapping, London",
-		hostedBy: 'Bob',
-		hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
-		attendees: [
-			{
-				id: 'a',
-				name: 'Bob',
-				photoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
-			},
-			{
-				id: 'b',
-				name: 'Tom',
-				photoURL: 'https://randomuser.me/api/portraits/men/22.jpg',
-			},
-		],
-	},
-	{
-		id: '2',
-		title: 'Trip to Punch and Judy Pub',
-		date: '2018-03-28T14:00:00+00:00',
-		category: 'drinks',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
-		city: 'London, UK',
-		venue: 'Punch & Judy, Henrietta Street, London, UK',
-		hostedBy: 'Tom',
-		hostPhotoURL: 'https://randomuser.me/api/portraits/men/22.jpg',
-		attendees: [
-			{
-				id: 'b',
-				name: 'Tom',
-				photoURL: 'https://randomuser.me/api/portraits/men/22.jpg',
-			},
-			{
-				id: 'a',
-				name: 'Bob',
-				photoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
-			},
-		],
-	},
-];
-
 class EventDashboard extends Component {
 	state = {
-		events,
 		isOpen: false,
 	};
 
@@ -65,23 +14,28 @@ class EventDashboard extends Component {
 	};
 
 	render() {
-		const { events, isOpen } = this.state;
+		const { isOpen } = this.state;
+		const { events } = this.props;
 		return (
-				<Grid stackable columns={2}>
-					<Grid.Column width={10}>
-						<EventList events={events} />
-					</Grid.Column>
-					<Grid.Column width={6}>
-						<Button
-							onClick={this.handleIsOpenToggle}
-							positive
-							content="Create Event"
-						/>
-						{isOpen && <EventForm cancelFormInput={this.handleIsOpenToggle} />}
-					</Grid.Column>
-				</Grid>
+			<Grid stackable columns={2}>
+				<Grid.Column width={10}>
+					<EventList events={events} />
+				</Grid.Column>
+				<Grid.Column width={6}>
+					<Button
+						onClick={this.handleIsOpenToggle}
+						positive
+						content="Create Event"
+					/>
+					{isOpen && <EventForm cancelFormInput={this.handleIsOpenToggle} />}
+				</Grid.Column>
+			</Grid>
 		);
 	}
 }
 
-export default EventDashboard;
+const mapStateToProps = state => ({
+	events: state.events,
+});
+
+export default connect(mapStateToProps)(EventDashboard);
--- src/features/events/EventList/EventList.jsx
+++ src/features/events/EventList/EventList.jsx
@@ -1,16 +1,12 @@
 import React, { Component, Fragment } from 'react';
 import EventListItem from './EventListItem';
 
-class EventList extends Component {
-	render() {
-		return (
-			<Fragment>
-				{this.props.events.map(event => (
-					<EventListItem key={event.id} event={event} />
-				))}
-			</Fragment>
-		);
-	}
-}
+const EventList = ({ events }) => (
+	<Fragment>
+		{events && events.map(event => (
+			<EventListItem key={event.id} event={event} />
+		))}
+	</Fragment>
+);
 
 export default EventList;
