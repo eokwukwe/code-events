703488db21e592927738c60de3554562c7adb68a
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2019-12-20 18:57:21 +0100

    feature: add facebook login

--- src/features/auth/Login/LoginForm.jsx
+++ src/features/auth/Login/LoginForm.jsx
@@ -4,7 +4,7 @@ import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import { combineValidators, isRequired } from 'revalidate';
 
-import { login } from '../authActions';
+import { login, socialLogin } from '../authActions';
 import TextInput from '../../../app/common/form/TextInput';
 import SocialLogin from '../SocialLogin/SocialLogin';
 
@@ -13,42 +13,49 @@ const validate = combineValidators({
 	password: isRequired('password'),
 });
 
-const LoginForm = ({ login, handleSubmit, error, invalid, submitting }) => {
+const LoginForm = ({
+	login,
+	handleSubmit,
+	error,
+	invalid,
+	submitting,
+	socialLogin,
+}) => {
 	return (
 		<Form onSubmit={handleSubmit(login)} error size="large">
-				<Field
-					name="email"
-					component={TextInput}
-					type="email"
-					placeholder="Email Address"
-				/>
-				<Field
-					name="password"
-					component={TextInput}
-					type="password"
-					placeholder="password"
-				/>
-				{error && (
-					<Label style={{ marginBottom: '0.6rem' }} basic color="red">
-						{error}
-					</Label>
-				)}
-				<Button
-					disabled={invalid || submitting}
-					loading={submitting}
-					fluid
-					size="medium"
-					color="teal"
-				>
-					Login
-				</Button>
-				<Divider horizontal>OR</Divider>
-				<SocialLogin />
+			<Field
+				name="email"
+				component={TextInput}
+				type="email"
+				placeholder="Email Address"
+			/>
+			<Field
+				name="password"
+				component={TextInput}
+				type="password"
+				placeholder="password"
+			/>
+			{error && (
+				<Label style={{ marginBottom: '0.6rem' }} basic color="red">
+					{error}
+				</Label>
+			)}
+			<Button
+				disabled={invalid || submitting}
+				loading={submitting}
+				fluid
+				size="medium"
+				color="teal"
+			>
+				Login
+			</Button>
+			<Divider horizontal>OR</Divider>
+			<SocialLogin socialLogin={socialLogin} />
 		</Form>
 	);
 };
 
-const actions = { login };
+const actions = { login, socialLogin };
 
 export default connect(
 	null,
--- src/features/auth/SocialLogin/SocialLogin.jsx
+++ src/features/auth/SocialLogin/SocialLogin.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, Icon } from 'semantic-ui-react';
 
-const SocialLogin = () => {
+const SocialLogin = ({ socialLogin }) => {
 	return (
 		<div>
 			<Button
@@ -10,6 +10,7 @@ const SocialLogin = () => {
 				fluid
 				size="medium"
 				color="facebook"
+				onClick={() => socialLogin('facebook')}
 			>
 				<Icon name="facebook" /> Login with Facebook
 			</Button>
