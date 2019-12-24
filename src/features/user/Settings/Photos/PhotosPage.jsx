8bde4a2cb819eb3b4b7357a9a39c802d8a16aba7
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2019-12-24 21:12:38 +0100

    feature: add react dropzone for image upload

--- src/features/user/Settings/Photos/PhotosPage.jsx
+++ src/features/user/Settings/Photos/PhotosPage.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
 	Image,
 	Segment,
@@ -8,8 +8,18 @@ import {
 	Button,
 	Card,
 } from 'semantic-ui-react';
+import DropzoneInput from './DropzoneInput';
 
 const PhotosPage = () => {
+	const [files, setFiles] = useState([]);
+
+	useEffect(() => {
+		// Cleanup the object URL created in memory
+		return () => {
+			files.forEach(file => URL.revokeObjectURL(file.preview));
+		};
+	}, [files]);
+
 	return (
 		<Segment>
 			<Header dividing size="large" content="Your Photos" />
@@ -17,6 +27,7 @@ const PhotosPage = () => {
 				<Grid.Row />
 				<Grid.Column width={4}>
 					<Header color="teal" sub content="Step 1 - Add Photo" />
+					<DropzoneInput setFiles={setFiles} />
 				</Grid.Column>
 				<Grid.Column width={1} />
 				<Grid.Column width={4}>
@@ -24,7 +35,13 @@ const PhotosPage = () => {
 				</Grid.Column>
 				<Grid.Column width={1} />
 				<Grid.Column width={4}>
-					<Header sub color="teal" content="Step 3 - Preview and Upload" />
+					<Header sub color="teal" content="Step 3 - Preview & Upload" />
+					{files.length > 0 && (
+						<Image
+							src={files[0].preview}
+							style={{ minHeight: '200px', minWidth: '200px' }}
+						/>
+					)}
 				</Grid.Column>
 			</Grid>
 
@@ -34,9 +51,7 @@ const PhotosPage = () => {
 			<Card.Group itemsPerRow={5}>
 				<Card>
 					<Image src="https://randomuser.me/api/portraits/men/20.jpg" />
-					<Button positive>
-						Main Photo
-					</Button>
+					<Button positive>Main Photo</Button>
 				</Card>
 
 				<Card>
