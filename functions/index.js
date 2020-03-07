4dddaced6dc1f01aa1b4ca85f4bf550fa63dd419
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2020-03-08 00:18:47 +0100

    feature: add cancelActivity cloud function

--- functions/index.js
+++ functions/index.js
@@ -3,25 +3,25 @@ const admin = require('firebase-admin');
 
 admin.initializeApp(functions.config().firebase);
 
+const newActivity = (type, event, id) => {
+  return {
+    type,
+    eventDate: event.date,
+    hostedBy: event.hostedBy,
+    title: event.title,
+    photoURL: event.hostPhotoURL,
+    timestamp: admin.firestore.FieldValue.serverTimestamp(),
+    hostUid: event.hostUid,
+    eventId: id,
+  };
+};
+
 exports.createActivity = functions.firestore
   .document('events/{eventId}')
   .onCreate(event => {
     let newEvent = event.data();
 
-    console.log('new eventtttttttt', newEvent);
-
-    const activity = {
-      type: 'newEvent',
-      eventDate: newEvent.date,
-      hostedBy: newEvent.hostedBy,
-      title: newEvent.title,
-      photoURL: newEvent.hostPhotoURL,
-      timestamp: admin.firestore.FieldValue.serverTimestamp(),
-      hostUid: newEvent.hostUid,
-      eventId: event.id,
-    };
-
-    console.log('activityyyyyyyyyy', activity);
+    const activity = newActivity('newEvent', newEvent, event.id);
 
     return admin
       .firestore()
@@ -32,5 +32,35 @@ exports.createActivity = functions.firestore
       })
       .catch(error => {
         return console.log('Error adding activity', error);
+      });
+  });
+
+exports.cancelActivity = functions.firestore
+  .document('events/{eventId}')
+  .onUpdate((event, context) => {
+    const updatedEvent = event.after.data();
+    const previousEventData = event.before.data();
+
+    if (
+      !updatedEvent.cancelled ||
+      updatedEvent.cancelled === previousEventData.cancelled
+    )
+      return false;
+
+    const activity = newActivity(
+      'cancelledEvent',
+      updatedEvent,
+      context.params.eventId,
+    );
+
+    return admin
+      .firestore()
+      .collection('activity')
+      .add(activity)
+      .then(docRef => {
+        return console.log('Activity cancelled with ID', docRef.id);
+      })
+      .catch(error => {
+        return console.log('Error cancelling activity', error);
       });
   });
